4c766c10d448f983462c651a33dbab014aaad29f
schliefa <100690630+schliefa@users.noreply.github.com>
2022-03-17 17:26:06 +0100

    Add files via upload

--- matlab/nifti_import.m
+++ matlab/nifti_import.m
@@ -1,24 +1,29 @@
-% this code can load a single nifti image
-% .nii and .nii.gz
-
-
+% this code can l~oad a single nifti image
 % in order to use this function download 
 % https://www.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image
 
-clear all;
+function cube_all=nifti_import
 
-% path = 'C:\Users\matth\OneDrive\Desktop\work\Hackathon\data\mre_original_nii\IRMRE\'
-% nii_file = 'f191125112201STD1312211075243167072-0045-00001-000001-01.nii'
+path = 'C:\Users\adria\Documents\Hackathon\P5-Elastography\data\mre_original_nii\IRMRE\';
+files_nii = dir([path,'*.nii']);
+N=length(files_nii);
+cube_all=zeros(N,110,108,5);
+%%
 
-path = 'C:\Users\matth\OneDrive\Desktop\work\Hackathon\data\mre_smoothed\'
-nii_file = 'aphif1.nii.gz'
+    for i=1:N
+    nii_file = files_nii(i).name;
 
+    file_path = strcat(path,nii_file);
 
-file_path = strcat(path,nii_file);
+    nii_data = load_nii(file_path);
 
-nii_data = load_nii(file_path);
+    cube = double(nii_data.img);
+    cube = rescale(cube, -pi, pi); % rescale for unwrapping
 
-cube = double(nii_data.img);
-cube = rescale(cube, -pi, pi); % rescale for unwrapping
+    % imshow(cube(:,:,2), [-pi pi])
 
-imshow(cube(:,:,2), [-pi pi])
+    cube_all(i,:,:,:)=cube;
+    end
+    cube_all = permute (cube_all, [2 3 4 1]);
+
+end
